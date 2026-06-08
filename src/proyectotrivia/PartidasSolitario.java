da31ef9265cff6bb706c1cf1c6cfd5ceaf71d19c
Jero <jeronimolperin@gmail.com>
2026-06-08 02:06:57 -0300

    pregunta MC funcionando, falta verificacion respuesta

--- src/proyectotrivia/PartidasSolitario.java
+++ src/proyectotrivia/PartidasSolitario.java
@@ -17,7 +17,7 @@ public class PartidasSolitario extends Partidas {
     int idUsuario;
     int puntajeFinal;
     int indicePregunta;
-    Preguntas[] preguntasPartida = new PreguntasVerdaderoFalso[5] ;
+    Preguntas[] preguntasPartida = new Preguntas[5] ;
     Respuestas[] respuestasPartida = new Respuestas[5];
     
     
@@ -64,31 +64,88 @@ public class PartidasSolitario extends Partidas {
                     "root",
                     ""
             );
-        String sql = "SELECT * FROM preguntas p JOIN preguntas_vf v ON p.idPregunta = v.idPregunta ORDER BY RAND()LIMIT 5";
+       
+        
+                String sql = "SELECT * FROM preguntas ORDER BY RAND() LIMIT 5";
 
         Statement st = con.createStatement();
-
         ResultSet rs = st.executeQuery(sql);
-        
+
+        int pos = 0;
+
         while(rs.next()){
-            //boolean opcionCorrecta, int idPregunta, int idCategoria, String enunciado, int tipoPregunta, int dificultad
 
-            //aca vendria el switch dependiendo el tipo de pregunta, por ahora solo VoF
-           boolean op = rs.getBoolean("opcionCorrecta");
-           int idPregunta = rs.getInt("idPregunta");
-           int idCategoria = rs.getInt("idCategoria");
-           int dificultad = rs.getInt("dificultad");
-           String enunciado = rs.getString("enunciado");
-           int tipo = rs.getInt("tipo");
+            int idPregunta = rs.getInt("idPregunta");
+            int idCategoria = rs.getInt("idCategoria");
+            int dificultad = rs.getInt("dificultad");
+            String enunciado = rs.getString("enunciado");
+            int tipo = rs.getInt("tipo");
 
-           Preguntas p = new PreguntasVerdaderoFalso(op,idPregunta,idCategoria,enunciado,tipo,dificultad);
-           //Guardamos la pregunta en el array 
-            for(int i =0;i<preguntasPartida.length;i++)
-                preguntasPartida[i] = p;
+            System.out.println("ID = " + idPregunta);
+        System.out.println("TIPO = " + tipo);
+            
+            switch(tipo){
+
+                case 1: // Verdadero/Falso
+
+                    Statement stVF = con.createStatement();
+
+                    String sqlVF =  "SELECT * FROM preguntas_vf " + "WHERE idPregunta = " + idPregunta;
+
+                    ResultSet rsVF = stVF.executeQuery(sqlVF);
+
+                    if(rsVF.next()){
+                            System.out.println("Encontrada pregunta hija vf");
+                        boolean opcionCorrecta =
+                            rsVF.getBoolean("opcionCorrecta");
+
+                        preguntasPartida[pos] = new PreguntasVerdaderoFalso(opcionCorrecta, idPregunta, idCategoria, enunciado,tipo,dificultad);
+                    }
+
+                    break;
+
+                case 2: // Multiple Choice
+                    
+                    Statement stMC = con.createStatement();
+
+                    String sqlMC =
+                        "SELECT * FROM preguntasmultiplechoice " + "WHERE idPregunta = " + idPregunta;
+                        System.out.println(sqlMC);
+                    ResultSet rsMC = stMC.executeQuery(sqlMC);
+
+                    if(rsMC.next()){
+                            System.out.println("Encontrada pregunta mc");
+                        String opcion1 = rsMC.getString("opcion1");
+                        String opcion2 = rsMC.getString("opcion2");
+                        String opcion3 = rsMC.getString("opcion3");
+                        String opcion4 = rsMC.getString("opcion4");
+                        String opcionCorrecta = rsMC.getString("opcionCorrecta");
+
+                        preguntasPartida[pos] =
+                            new PreguntasMultipleChoice(
+                                opcion1,
+                                opcion2,
+                                opcion3,
+                                opcion4,
+                                opcionCorrecta,
+                                idPregunta,
+                                idCategoria,
+                                enunciado,
+                                tipo,
+                                dificultad
+                            );
+                    }
+
+                    break;
+            }
+
+            pos++;
         }
         puntajeFinal=0;
-        indicePregunta=0;
-        
+        indicePregunta=5;
+        for(int i = 0; i < preguntasPartida.length; i++){
+    System.out.println(i + " -> " + preguntasPartida[i]);
+}
         
         
         } catch (Exception e) {
@@ -100,7 +157,7 @@ public class PartidasSolitario extends Partidas {
     
     };
     
-    public void aumentarIndice(){ indicePregunta++;}
+    
     
     public void finalizarPartida(){
     
@@ -111,6 +168,8 @@ public class PartidasSolitario extends Partidas {
     };
     public Preguntas siguientePregunta(){
         
+       
+       indicePregunta--; 
      return preguntasPartida[indicePregunta];
         
     
