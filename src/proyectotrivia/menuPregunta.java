5350b1d24c217a69eb4d9fe8996a84366c75aa46
Jero <jeronimolperin@gmail.com>
2026-06-08 01:20:14 -0300

    creacion Panel MC y estructura basica switch

--- src/proyectotrivia/menuPregunta.java
+++ src/proyectotrivia/menuPregunta.java
@@ -39,6 +39,24 @@ public class menuPregunta extends javax.swing.JFrame {
     
     private void mostrarPregunta(){
         
+        
+        panelVerdaderoFalso.setVisible(false);
+        
+        
+        switch(pregActual.tipoPregunta){
+        
+           case 1 : panelVerdaderoFalso.setVisible(true);
+           
+           case 2 : 
+               
+            //   panelMultipleChoice.setVisible(true);
+           
+           
+           case 3 : ;
+           
+        
+        }
+        
         System.out.println("Mostrando: " + pregActual.enunciado);
         lblPregunta.setText(pregActual.enunciado);
     }
@@ -52,6 +70,11 @@ public class menuPregunta extends javax.swing.JFrame {
         panelVerdaderoFalso = new javax.swing.JPanel();
         btnVerdadero = new javax.swing.JButton();
         btnFalso = new javax.swing.JButton();
+        jPanel1 = new javax.swing.JPanel();
+        opcion3MultipleChoice = new javax.swing.JButton();
+        opcion1MultipleChoice = new javax.swing.JButton();
+        opcion2MultipleChoice = new javax.swing.JButton();
+        opcion4MultipleChoice = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -59,6 +82,7 @@ public class menuPregunta extends javax.swing.JFrame {
         btnVerdadero.addActionListener(this::btnVerdaderoActionPerformed);
 
         btnFalso.setText("FALSO");
+        btnFalso.addActionListener(this::btnFalsoActionPerformed);
 
         javax.swing.GroupLayout panelVerdaderoFalsoLayout = new javax.swing.GroupLayout(panelVerdaderoFalso);
         panelVerdaderoFalso.setLayout(panelVerdaderoFalsoLayout);
@@ -81,26 +105,69 @@ public class menuPregunta extends javax.swing.JFrame {
                 .addGap(31, 31, 31))
         );
 
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(55, 55, 55)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(opcion2MultipleChoice)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 144, Short.MAX_VALUE)
+                        .addComponent(opcion4MultipleChoice))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(opcion1MultipleChoice)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(opcion3MultipleChoice)))
+                .addGap(52, 52, 52))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap(17, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(opcion3MultipleChoice, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(17, 17, 17))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(opcion1MultipleChoice, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(opcion2MultipleChoice, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(opcion4MultipleChoice, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap())
+        );
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(68, 68, 68)
-                .addComponent(lblPregunta, javax.swing.GroupLayout.PREFERRED_SIZE, 272, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(60, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(panelVerdaderoFalso, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(68, 68, 68)
+                        .addComponent(lblPregunta, javax.swing.GroupLayout.PREFERRED_SIZE, 685, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 27, Short.MAX_VALUE)))
+                .addContainerGap())
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(panelVerdaderoFalso, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(180, 180, 180)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(17, 17, 17)
                 .addComponent(lblPregunta, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 66, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 56, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(26, 26, 26)
                 .addComponent(panelVerdaderoFalso, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(62, 62, 62))
+                .addGap(20, 20, 20))
         );
 
         pack();
@@ -110,6 +177,10 @@ public class menuPregunta extends javax.swing.JFrame {
         procesarRespuesta("TRUE");
     }//GEN-LAST:event_btnVerdaderoActionPerformed
 
+    private void btnFalsoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnFalsoActionPerformed
+       procesarRespuesta("FALSE");
+    }//GEN-LAST:event_btnFalsoActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -118,7 +189,12 @@ public class menuPregunta extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnFalso;
     private javax.swing.JButton btnVerdadero;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel lblPregunta;
+    private javax.swing.JButton opcion1MultipleChoice;
+    private javax.swing.JButton opcion2MultipleChoice;
+    private javax.swing.JButton opcion3MultipleChoice;
+    private javax.swing.JButton opcion4MultipleChoice;
     private javax.swing.JPanel panelVerdaderoFalso;
     // End of variables declaration//GEN-END:variables
 }
